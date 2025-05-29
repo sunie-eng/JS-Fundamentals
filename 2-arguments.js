28a489971d315b0f0cd91ea929cb6ae519413d97
sunie-eng <odoh619@gmail.com>
2025-05-29 12:34:13 -0700

    Delete 2-arguments.js

--- 2-arguments.js
+++
@@ -1,8 +0,0 @@
-const args = process.argv.slice(2);
-if (args.length ===0){
-    console.log('No Argument');
-}else if (args.length ===1){
-    console.log('Argument found');
-} else {console.log('Arguments found');
-    
-}
