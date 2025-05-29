69645849f8a37dc1454c8a6b9d288f1660ce7910
sunie-eng <odoh619@gmail.com>
2025-05-29 02:51:22 -0700

    added my third JS file

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = Process.argv.slice();
+if (args.length ===0){
+    console.log('No Argument');
+}else if (args.length ===1){
+    console.log('Argument found');
+} else {console.log('Arguments found');
+    
+}
